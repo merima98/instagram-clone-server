516e612da96cf3cbd15c532520d574b09073452b
merima98 <merimaceranic98@gmail.com>
2021-01-31 22:44:00 +0100

    Adding update user info functionality

--- src/components/users/userController.js
+++ src/components/users/userController.js
@@ -1,5 +1,4 @@
 import usersDAL from "../users/usersDAL.js";
-import utils from "../../utils/index.js";
 
 async function getUser(req, res) {
   try {
@@ -17,6 +16,63 @@ async function getUser(req, res) {
   } catch (err) {}
 }
 
+async function getUserById(req, res) {
+  try {
+    const userId = res.locals.userId;
+    const user = await usersDAL.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    if (user === null) {
+      return res.status(400).send({ exception: "UserNotFound" });
+    }
+
+    const response = {
+      email: user.email,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      image: user.image,
+    };
+    return res.status(200).send(response);
+  } catch (err) {}
+}
+
+async function patchMySelf(req, res) {
+  try {
+    const userId = res.locals.userId;
+    const values = req.body;
+
+    const existingUsername = await usersDAL.findOne({
+      where: { username: req.body.username },
+    });
+    if (existingUsername !== null && existingUsername.id !== userId) {
+      return res
+        .status(400)
+        .send({ exception: "UsernameAllreadyInUseException" });
+    }
+
+    const args = {
+      select: {
+        email: true,
+        firstName: true,
+        lastName: true,
+        username: true,
+        image: true,
+      },
+      where: {
+        id: userId,
+      },
+      data: { ...values },
+    };
+
+    const user = await usersDAL.updateUser(args);
+    return res.status(200).send(user);
+  } catch (error) {}
+}
 export default {
   getUser,
+  getUserById,
+  patchMySelf,
 };
--- src/components/users/userRoutes.js
+++ src/components/users/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 
 import userController from "./userController.js";
+import middlewares from "../../middlewares/requireAuthentication.js";
 
 const router = express.Router();
 
 router.get("/user", userController.getUser);
+router.get("/userById", middlewares, userController.getUserById);
+router.patch("/update", middlewares, userController.patchMySelf);
 
 export default router;
